4fff8d10bc4f51273cd0ff50263797f1b4017536
Samarth Singh Chauhan <162859294+SamaChauhan@users.noreply.github.com>
2026-06-02 23:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Generated from: app.ipynb
-# Converted at: 2026-06-02T17:26:51.607Z
-# Next step (optional): refactor into modules & generate tests with RunCell
-# Quick start: pip install runcell
-
 from flask import Flask, jsonify
 import random
 
